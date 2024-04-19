7409b5753c57566573200ae9f8573752e48ee786
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-04-19 14:11:06 +0000

    DataType

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+//"use strict"; // treat all JS code as newer version
+
+// alert ( 3 + 3 ) // we are using nodejs not browser
+
+console.log (3 + 3);
+
+console.log ("Shubham Shinde");//code readablity should be high
+
+
+let name = "shubham" //String
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => "value "
+// boolean => true/false
+// null => standalone value  (its an object data type  Null means empty temporature aya hi nhi )
+// undefined => 
+// symbol => unique
+
+//object 
+
+console.log (typeof "shubham");
+console.log (typeof undefined);
+console.log(typeof null);
+
+
+
+
