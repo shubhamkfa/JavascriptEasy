eb9b0591264a3047e93de5397a96597e4cb17174
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-04-20 15:15:09 +0000

    Strings started not completed yet

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "Shubham"
+const repoCount = 20
+
+console.log ( name + repoCount + " Value"); //this is the old dated concatinate option 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount} .`); // we can user backtick and 
+//doller and curly braces this is new one 
+// we can add method while in go 
+
+
+const gameName = new String('shubhamshinde')
