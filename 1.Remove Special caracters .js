f16db970f457241b4d303f0b6b7cefa3f1a9dee5
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-08-06 23:59:33 +0530

    Add files via upload

---
+++ 1.Remove Special caracters .js
@@ -0,0 +1,9 @@
+var str="#6%&shuBh&shuba517723)->?<':{sdlkfkhaoi";
+
+// we can remove all special character by using below method 
+console.log(str.replace(/[^a-zA-Z0-9]/g,''))
+
+
+
+// console.log(ab.replace(/[^a-zA-Z0-9]/g, ""))
+// replace(/[^a-zA-Z0-9]/g,"")
