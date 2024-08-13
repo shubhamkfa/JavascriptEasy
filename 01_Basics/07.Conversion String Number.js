7157623b3018f69b927172c457c4b7b4bee8a45d
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-08-13 14:26:11 +0000

    Got it

---
+++ 01_Basics/07.Conversion String Number.js
@@ -0,0 +1,41 @@
+// ******************** Operations ***************//
+/*
+let value = 3 
+let negValue = -value
+
+// console.log(negValue)
+console.table([value, negValue, typeof negValue])
+
+let str1 ="hello"
+let str2 = " Shubham"
+let str3 = str1 + str2
+console.log(str3);
+
+
+
+console.log("1" + 2 )
+console.log (1+"2") 
+console.log("1" + 2 + 2);
+console.log( 1 + 2 + "2");
+
+console.log(+true)
+console.log(+"")
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+console.log(num2);
+
+*/
+
+//*******************++ Prefix ,  Postfix ++  */
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounter2 = 99
+++gameCounter2;
+console.log(gameCounter2);
+
---
+++ 01_Basics/08.Comparision Data Type.js
@@ -0,0 +1,41 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 2);
+
+
+
+//Before comparision make sure you have changed the variable datatype 
+
+
+
+console.log("2" > 3);
+console.log("02" > 1);
+
+//Comparision and equality is two different thing in Javascript 
+
+
+console.log(null > 0);
+console.log(null == 0); 
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined == 1);
+
+console.log(undefined == NaN);
+
+
+//Strick check  ===
+
+console.log("02" === 2);
+
+console.log("02" == 2); // without strickt it will not chekc data type 
+
+
+// Note: always write Clean code 
+
+
+
+
